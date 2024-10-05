c9eeef1806622880773e5a06b3b57f8929dfcc86
joshuaApostol2 <joshuaapostol0967@gmail.com>
2024-10-05 22:21:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const port = 3000;
 const blocklist = new Set();
 const CLOUDFLARE_API_KEY = 'jR_QLJkhgWLtCJsMRFHUtnMIoQzSO4PNyJ_AwiWZ';
 const CLOUDFLARE_ZONE_ID = '4de7cfa4c579eba6a1bc257bf61b9c6e';
-const MAX_REQUESTS = 100;
+const MAX_REQUESTS = 15;
 const CONNECTION_LIMIT = 100;
 
-const ipRequestCount = {};
-const ipConnectionCount = {};
+const totalRequestCount = { count: 0 };
 
 const verifyCloudflareToken = async () => {
     try {
@@ -55,7 +54,7 @@ const rateLimiter = rateLimit({
     windowMs: 1 * 60 * 1000,
     max: MAX_REQUESTS,
     handler: (req, res) => {
-        res.status(403).send('hina ng DDoS mo bata HAHAHAHA');
+        res.status(403).send('Hina ng DDoS mo, bata HAHAHAHA');
     },
 });
 
@@ -63,33 +62,24 @@ app.use(rateLimiter);
 
 app.use(async (req, res, next) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    
+
     if (isBannedIP(ip)) {
-        return res.status(403).send('hina ng DDoS mo bata HAHAHAHA');
+        return res.status(403).send('Hina ng DDoS mo, bata HAHAHAHA');
     }
 
-    ipRequestCount[ip] = (ipRequestCount[ip] || 0) + 1;
-    ipConnectionCount[ip] = (ipConnectionCount[ip] || 0) + 1;
+    totalRequestCount.count++;
 
-    if (ipRequestCount[ip] > MAX_REQUESTS || ipConnectionCount[ip] > CONNECTION_LIMIT) {
+    if (totalRequestCount.count > MAX_REQUESTS) {
         blocklist.add(ip);
         await blockIPCloudflare(ip);
-        return res.status(403).send('hina ng DDoS mo bata HAHAHAHA');
+        return res.status(403).send('Hina ng DDoS mo, bata HAHAHAHA');
     }
-
-    setTimeout(() => {
-        ipRequestCount[ip]--;
-    }, 60000);
-
-    setTimeout(() => {
-        ipConnectionCount[ip]--;
-    }, 60000);
 
     next();
 });
 
 app.get('/', async (req, res) => {
-    res.send('hina ng DDoS mo bata HAHAHAHA');
+    res.send('Welcome to the secure API! Hina ng DDoS mo, bata HAHAHAHA');
 });
 
 app.use((req, res) => {
